a9a09a5f65500f68b1b2513991a9b77e27436b49
Ifeanyi Ogbonna <ogbonna.ifybest@gmail.com>
2023-02-10 20:26:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import List from "./List";
+
 
 function App() {
+  const [currentItem, setCurrentItem] = useState(null);
+  const [itemList, updateItemList] = useState([]);
+
+  const onChangeHandler = e => {
+    setCurrentItem(e.target.value);
+  };
+
+  const addTodoList = () => {
+    updateItemList([...itemList, {item :currentItem, key: Date.now() }]);
+    setCurrentItem("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <div className="input-wrapper">
+            <input value={currentItem} onChange={onChangeHandler} type="text"  placeholder="Enter Text" />
+            <button onClick={addTodoList} type="submit"> Add </button>
+          </div>
+          <List itemList={itemList} updateItemList={updateItemList} />
+        </div>
       </header>
     </div>
   );
---
+++ src/List.js
@@ -0,0 +1,24 @@
+import "./List.css";
+
+function List({itemList, updateItemList}) {
+  const deleteItem = key => {
+    const newList = itemList.filter(itemObj => {
+      return itemObj.key !== key;
+    });
+    updateItemList(newList);
+  };
+  return (
+    <div>
+      {itemList.map((itemObj) => {
+        return (
+          <div key={itemObj.key}className="Item">
+            <p>{itemObj.item}</p>
+            <button onClick={() => deleteItem(itemObj.key)}>&times;</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default List;
